84b85624048a68d87fc93b8d497c9886ba8e9ddb
derpwanda <derpwanda@gmail.com>
2024-08-18 17:24:26 -0500

    initialize project

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+def main():
+    # Create a new TextNode object with dummy values
+    node = TextNode("Hello, world!", "italics", "https://google.com")
+    
+    # Print the object
+    print(node)
+
+if __name__ == "__main__":
+    main()
